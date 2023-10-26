238a146727e2b433eb83165f9c0b17a21ee28bb7
antoniehuang <Antonie.Huang@sainsburys.co.uk>
2023-10-26 16:06:47 +0100

    Create source s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,11 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
+resource "aws_s3_bucket" "source_bucket" {
+  bucket = "intro-pipeline-task-source-terraform"
+
+  tags = {
+    project_name = "Introductory Pipeline Task"
+  }
+}
