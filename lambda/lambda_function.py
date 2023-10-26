3ec727f9d664cc8ac309d7df9f8c69f59cacf62f
antoniehuang <Antonie.Huang@sainsburys.co.uk>
2023-10-26 14:54:12 +0100

    Copy file from source bucket to target(datalake) bucket

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,8 +1,11 @@
 import json
 import logging
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+s3 = boto3.resource("s3")
 
 
 def lambda_handler(event, context):
@@ -14,6 +17,13 @@ def lambda_handler(event, context):
 
     key = body["Records"][0]["s3"]["object"]["key"]
     logger.info("Object's Key: %s", key)
+
+    copy_source = {"Bucket": source_bucket_name, "Key": key}
+
+    target_bucket_name = "intro-pipeline-task-datalake"
+
+    response = s3.meta.client.copy(copy_source, target_bucket_name, key)
+    logger.info("S3 Client response: %s", response)
 
     return {
         "statusCode": 200,
