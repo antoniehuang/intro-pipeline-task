4137960ea6739b44864f362a7295e0c5ab773305
antoniehuang <Antonie.Huang@sainsburys.co.uk>
2023-10-27 15:01:07 +0100

    Add source bucket event notification to publish to sns on new ObjectCreated

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,6 +6,15 @@ resource "aws_s3_bucket" "source_bucket" {
   }
 }
 
+resource "aws_s3_bucket_notification" "source_bucket_notification" {
+  bucket = aws_s3_bucket.source_bucket.id
+
+  topic {
+    topic_arn = aws_sns_topic.source_bucket_event_sns_topic.arn
+    events    = ["s3:ObjectCreated:*"]
+  }
+}
+
 resource "aws_s3_bucket" "datalake_bucket" {
   bucket = "intro-pipeline-task-datalake-terraform"
 
