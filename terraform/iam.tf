6650ffd3b5d27810c2551b6a6adc5ffc4caf3195
antoniehuang <Antonie.Huang@sainsburys.co.uk>
2023-10-27 16:09:30 +0100

    Create cloudwatch group and add policies to lambda

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_role" "pipeline_lambda_role" {
+  name               = "intro-pipeline-task-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -11,7 +16,74 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "pipeline_lambda_role" {
-  name               = "intro-pipeline-task-lambda-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+data "aws_iam_policy_document" "lambda_sqs_read_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:ReceiveMessage"
+    ]
+
+    resources = [aws_sqs_queue.source_bucket_event_sqs_queue.arn]
+  }
+}
+
+resource "aws_iam_policy" "lambda_sqs_read_policy" {
+  name        = "lambda_sqs_read_policy"
+  path        = "/"
+  description = "IAM policy for lambda to read from sqs"
+  policy      = data.aws_iam_policy_document.lambda_sqs_read_policy.json
+}
+
+data "aws_iam_policy_document" "lambda_bucket_read_write_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+
+    resources = [aws_s3_bucket.source_bucket.arn]
+  }
+}
+
+resource "aws_iam_policy" "lambda_bucket_read_write_policy" {
+  name        = "lambda_sqs_read_policy"
+  path        = "/"
+  description = "IAM policy for lambda to read and write to s3"
+  policy      = data.aws_iam_policy_document.lambda_bucket_read_write_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policies_attachment" {
+  role = aws_iam_role.pipeline_lambda_role.name
+
+  for_each = toset([
+    aws_iam_policy.lambda_logging.arn,
+    aws_iam_policy.lambda_sqs_read_policy.arn,
+    aws_iam_policy.lambda_bucket_read_write_policy.arn
+  ])
+
+  policy_arn = each.value
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -22,3 +22,8 @@ resource "aws_lambda_event_source_mapping" "sqs_lambda_trigger" {
   function_name    = aws_lambda_function.pipeline_lambda.arn
   batch_size       = 1
 }
+
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${aws_lambda_function.pipeline_lambda.function_name}"
+  retention_in_days = 14
+}
