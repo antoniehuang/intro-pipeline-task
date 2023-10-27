7f0866e03a9dc563eef1260c783288d40da9194f
antoniehuang <Antonie.Huang@sainsburys.co.uk>
2023-10-27 14:48:35 +0100

    Update SNS policy to allow s3 to publish to topic

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -5,3 +5,41 @@ resource "aws_sns_topic" "source_bucket_event_sns_topic" {
     project_name = "Introductory Pipeline Task"
   }
 }
+
+resource "aws_sns_topic_policy" "default" {
+  arn = aws_sns_topic.source_bucket_event_sns_topic.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions = [
+      "SNS:Publish"
+    ]
+
+    resources = [
+      aws_sns_topic.source_bucket_event_sns_topic.arn,
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = ["114888082308"]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.source_bucket.arn]
+    }
+
+  }
+}
