bb5cdbd728d1bedece39beada5e7d3ce9941f276
antoniehuang <Antonie.Huang@sainsburys.co.uk>
2023-10-27 14:54:02 +0100

    Use s3 as terrafrom backend to store tfstates

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 
   required_version = ">= 1.6.2"
+
+  backend "s3" {
+    bucket         = "intro-pipeline-task-infra-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "intro-pipeline-task-infra-lockid"
+  }
 }
 
 # Configure the AWS Provider
